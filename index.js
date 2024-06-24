3ecce182746d71797c0181d1cd32c0b40c8bbb1f
Justin <rayjustin481@gmail.com>
2024-06-24 17:14:04 +0300

    string-lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Justin Ray";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
